7924c1b90ab20221552f484519061ee0ed9045de
Dmitry127 <sseleznev55@gmail.com>
2021-09-27 14:57:41 +0300

    failed to commit files fix

---
+++ src/main/java/ru/dmitrys/web/secutiry/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package ru.dmitrys.web.secutiry;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
